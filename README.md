279813cf4bcbbbb780ac5324a1b49bdfe9e9b3da
Tony Poerio <tony@tonypoer.io>
2016-05-15 21:54:24 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Cracking the Coding Interview
+## Solutions Written in C
+
+### Why would anyone do this
+- Good practice
+- Have not yet found a full solution in set in C, so hopefully this helps someone else too
+
+### Notes
+- Work in progress
---
+++ chapter1/isUnique.c
@@ -0,0 +1,12 @@
+/*
+ *---------------------
+ *|  CTCI - Chapter 1 |
+ *---------------------
+ * Problem 1.1:  is Unique
+ *
+ *"Implement an algorithm to determine if a string has all unique characters.
+ *  What if you cannot use additional data structures?"
+ *
+ */
+
+

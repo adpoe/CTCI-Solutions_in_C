84c4419f123137f9c645a037519efc6f8b3866e2
Tony Poerio <tony@tonypoer.io>
2016-05-18 18:05:59 -0400

    added skeleton for palindrome permutation

---
+++ chapter1/palindromePermuation.c
@@ -0,0 +1,21 @@
+/*
+ *---------------------
+ *|  CTCI - Chapter 1 |
+ *---------------------
+ * Problem 1.4:  Palindrome Permuation
+ *
+ * "Given a string, write a function to check if it is a permutation of a palindrome.
+ *  A palindrome is a word of phrase that is the same forwards and backwards.
+ *  A permutation is a rearrangement of letters. The palindrome does not need to be limited
+ *  to just dictionary words."
+ *
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    // code here
+}

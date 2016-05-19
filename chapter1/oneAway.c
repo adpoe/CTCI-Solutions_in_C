dc99a6912d876ee34dc1a3003416ea80369e1301
Tony Poerio <tony@tonypoer.io>
2016-05-19 10:43:33 -0400

    setup for oneAway problem

---
+++ chapter1/oneAway.c
@@ -0,0 +1,26 @@
+/*
+ *---------------------
+ *|  CTCI - Chapter 1 |
+ *---------------------
+ * Problem 1.5:  One Away
+ *
+ * "There are 3 types of edits that can be performed on a string.
+ *   - insert a char
+ *   - remove a char
+ *   - replace a char
+ *
+ * Given two strings, write a function to check if they are 1 edit (or 0 edits) away"
+ *
+ *
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+    // Get command line inputs
+    char *str1 = argv[1];
+    char *str2 = argv[2];
+
+}

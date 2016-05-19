a57b866c7cf16c8cec4dd7345c6694e06d01815f
Tony Poerio <tony@tonypoer.io>
2016-05-19 10:55:04 -0400

    created prototypes for oneAway, decided on general strategy

--- chapter1/oneAway.c
+++ chapter1/oneAway.c
@@ -18,9 +18,23 @@
 #include <string.h>
 #include <stdio.h>
 
+// Setup TypeDef for bools
+typedef enum { false, true } bool;
+
+// Function prototypes
+int createBitVector(char *string);
+int toggle(int bitVector, int index);
+int checkIfLessThanOneDifference(char *str1, char *str2);
+
 int main(int argc, char *argv[]){
     // Get command line inputs
     char *str1 = argv[1];
     char *str2 = argv[2];
+
+    // idea:  convert each string to a bitVector, using the toggle method, like before
+    // then, AND the bitVectors
+    // then iterate through the bitVectors,
+    // and if more than number is different, we have don't have a "one away" match
+
 
 }

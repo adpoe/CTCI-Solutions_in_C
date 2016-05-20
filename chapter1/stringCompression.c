85047b4af7f9c11a28d03880363ab5925692817e
Tony Poerio <tony@tonypoer.io>
2016-05-20 00:28:25 -0400

    setup stringCompression

---
+++ chapter1/stringCompression.c
@@ -0,0 +1,26 @@
+/*
+ *---------------------
+ *|  CTCI - Chapter 1 |
+ *---------------------
+ * Problem 1.6:  String Compression
+ *
+ * "Implement a method to perform a basic string compression using the counts
+ * of repeated characters.
+ * - Example:  aabcccccaaa -> a2b2c5a3
+ * If the "compressed" string would not be a smaller than the original string,
+ * the method should return the original string. Can assume only letters (a-z)"
+ *
+ * */
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+// Setup TypeDef for bools
+typedef enum { false, true } bool;
+
+int main(int argc, char *argv[])
+{
+    // code goes here
+}
+

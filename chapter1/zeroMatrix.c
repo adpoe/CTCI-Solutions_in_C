a91746eb6276e56405302fba8503505a413d9176
Tony Poerio <tony@tonypoer.io>
2016-05-21 12:13:39 -0400

    setup zero matrix problem

---
+++ chapter1/zeroMatrix.c
@@ -0,0 +1,22 @@
+/*
+ *---------------------
+ *|  CTCI - Chapter 1 |
+ *---------------------
+ * Problem 1.8: Zero Matrix
+ *
+ *"Write an algorithm such that if an element in an MxN matrix is zero,
+* then its entire ROW and COLUMN are set to zero.""
+*
+ */
+
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    // get dimensions of matrix
+    int rowDim = atoi(argv[1]);
+    int colDim = atoi(argv[2]);
+}

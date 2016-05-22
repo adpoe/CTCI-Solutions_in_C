906c8fdf45e7e7a0958c487d37e0f34f9c380712
Tony Poerio <tony@tonypoer.io>
2016-05-22 00:38:52 -0400

    setup string rotation problem

---
+++ chapter1/stringRotation.c
@@ -0,0 +1,22 @@
+/*
+ *---------------------
+ *|  CTCI - Chapter 1 |
+ *---------------------
+ * Problem 1.9: String Rotation
+ *
+ * "String Rotation: Assume you have a method isSubstring() which checks if
+ * one word is a substring of another. Given two strings, s1 and s2,
+ * write code to check if s2 is a rotation of s1 using only one call to
+ * isSubstring()."
+ *
+ * Ex:  "waterbottle" is a rotation of "erbottlwat"
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    // code here
+}

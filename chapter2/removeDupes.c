bb5f0ee156081d2e6b2d202f11d573cc872cca1d
Tony Poerio <tony@tonypoer.io>
2016-05-22 21:01:57 -0400

    updated readme

---
+++ chapter2/removeDupes.c
@@ -0,0 +1,21 @@
+/*
+ *---------------------
+ *|  CTCI - Chapter 2 |
+ *---------------------
+ * Problem 2.1: Remove Dupes
+ *
+ * "Write code to remove dupes from an unsorted LinkedList."
+ *
+ * Follow up:
+ *
+ * "How would you do this if storing temps is not allowed"
+ * */
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    // remove dupes
+}

9f8daa3617e1fea780a91ec3d86ff5431a570977
Tony Poerio <tony@tonypoer.io>
2016-06-05 13:12:41 -0400

    finished 2.5, sum values in two linked lists

--- chapter2/sumLists.c
+++ chapter2/sumLists.c
@@ -41,20 +41,22 @@ int main(int argc, char *argv[]) {
     l2->next = NULL;
 
     // get values of numberOne at each decimal place using mods
-    int counter = 1;
+    int counterOne = 0;
     while (numberOne > 0) {
         int value = numberOne % 10;
         printf("value=%d\n",value);
         numberOne = numberOne/10;
         insert_list(&l1, value);
+        counterOne++;
     }
 
     // get values of numberOne at each decimal place using mods
-    counter = 1;
+    int counterTwo = 0;
     while (numberTwo > 0) {
         int value = numberTwo % 10;
         numberTwo = numberTwo/10;
         insert_list(&l2, value);
+        counterTwo++;
     }
 
     // iterate through and make sure the number came out okay
@@ -73,56 +75,33 @@ int main(int argc, char *argv[]) {
         iterator = iterator->next;
     }
 
-/*
-    // create linked list, 10 elements
-    list *l = malloc( sizeof( list ) );
-    l->next = NULL;
-    insert_list(&l, "Last");
-    insert_list(&l, "second to last");
-    insert_list(&l, "third to last");
-    insert_list(&l, "fourth to last");
-    insert_list(&l, "fifth to last");
-    insert_list(&l, "sixth to last");
-    insert_list(&l, "seventh to last");
-    insert_list(&l, "eighth to last");
-    insert_list(&l, "ninth to last");
-    insert_list(&l, "tenth to last");
-    printf("list is allocated\n");
+
+    // rebuild my number from the list itself
+    int listNumOne=0;
+    int listNumTwo=0;
 
 
-    // ASSUME:  We aren't keeping a count of the # items in the list
-    // iterate through the LL, and get a count of how many items, assuming we don't keep a count
-    int count = 0;
-    list *iterator = l;
-    printf("iterator starts at: %s\n", l->item);
+    iterator = l1;
+    printf("iterator starts at: %d\n", l1->item);
+    int placeValue = counterOne;
     while(iterator->next != NULL) {
-        printf("ITEM IS: %s\n", iterator->item);
-        count++;
+        listNumOne +=  iterator->item * power(10, placeValue - 1);
         iterator = iterator->next;
-        printf("count = %d\n", count);
+        printf("NUMBER IS: %d\n", listNumOne);
+        placeValue--;
     }
 
-    // Okay. Know that we have a count, we can iterate again to get a reference
-    // to the kth to last item, then remove it.
-    iterator = l;
-    while(count > kth) {
-        count--;
-        iterator = iterator->next;
-    }
-
-    printf("ended at: %s\n", iterator->item);
-    // delete the kth to last item, using our reference to it
-    printf("Deleting item #: %s\n", iterator->item);
-    delete_list(&l, iterator->item);
-
-    // and display everything, one last time to show that kth to last was deleted
-    printf("----LIST IS NOW-----\n");
-    iterator = l;
-
+    iterator = l2;
+    printf("iterator starts at: %d\n", l2->item);
+    placeValue = counterTwo;
     while(iterator->next != NULL) {
-        printf("ITEM=%s\n", iterator->item);
+        listNumTwo +=  iterator->item * power(10, placeValue - 1);
         iterator = iterator->next;
+        printf("NUMBER IS: %d\n", listNumTwo);
+        placeValue--;
     }
-*/
+    printf("listNumOne = %d\n", listNumOne);
+    printf("listNumTwo = %d\n", listNumTwo);
+    printf("Sum = %d\n", listNumOne + listNumTwo);
 
 }
